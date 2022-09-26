e4f5b5867915d01e238a95238ed3a2e78e00ebb9
Chamith Wijesooriya <102579206+ChamithEC@users.noreply.github.com>
2022-09-26 13:32:21 +0530

    Tailwind Installed

--- event-man-web-app/pages/index.js
+++ event-man-web-app/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 
+
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container ">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
