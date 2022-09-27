65b1552dd2344dcd1b2f8de17bfdfe8825f9e075
Chamith Wijesooriya <102579206+ChamithEC@users.noreply.github.com>
2022-09-28 00:23:40 +0530

    Test

--- event-man-web-app/components/Common/Headings.jsx
+++ event-man-web-app/components/Common/Headings.jsx
@@ -1,15 +1,9 @@
 import { SectionRow } from "../Layouts/sectionLayouts";
 
 export const MainHeading = ({ title }) => {
-  return (
-    <SectionRow
-      className={
-        "w-full h-[100px] text-center justify-evenly items-center bg-blue-600 "
-      }
-    >
-      <div className={"text-center text-sky-50 font-bold text-3xl uppercase"}>
-        {title}
-      </div>
-    </SectionRow>
-  );
+	return (
+		<SectionRow className={"w-full h-[100px] text-center justify-evenly items-center bg-blue-600 "}>
+			<div className={"text-center text-sky-50 font-bold text-3xl uppercase"}>{title}</div>
+		</SectionRow>
+	);
 };
